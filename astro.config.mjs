2e7543b27727e7bac2bb65ef02969b0d8bf8345d
ruben00001 <a.ruben00001@gmail.com>
2023-02-07 12:47:28 +0000

    work image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://virtualworks.io",
   base: "/",
   publicDir: "./public",
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
 });
