145d3375c3d704f540ad5f567b6c2e4f89826d8e
ruben00001 <a.ruben00001@gmail.com>
2023-02-09 13:54:59 +0000

    more slider

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -7,3 +7,13 @@ export function calcNextIndex({
 }) {
   return currentIndex + 1 <= maxIndex ? currentIndex + 1 : 0;
 }
+
+export function calcPrevIndex({
+  currentIndex,
+  maxIndex,
+}: {
+  currentIndex: number;
+  maxIndex: number;
+}) {
+  return currentIndex - 1 >= 0 ? currentIndex - 1 : maxIndex;
+}
