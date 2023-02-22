856436501a0a450b3bdb045cdd2930050c380d9e
ruben00001 <a.ruben00001@gmail.com>
2023-02-22 16:17:08 +0000

    landing box color change; asatic slide image update; debounce work slider scroll

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -17,3 +17,16 @@ export function calcPrevIndex({
 }) {
   return currentIndex - 1 >= 0 ? currentIndex - 1 : maxIndex;
 }
+
+export function debounce<Params extends any[]>(
+  func: (...args: Params) => any,
+  timeout: number
+): (...args: Params) => void {
+  let timer: ReturnType<typeof setTimeout>;
+  return (...args: Params) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, timeout);
+  };
+}
